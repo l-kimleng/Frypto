3ca031fecddf04ab0d171a3a9a9b0913eb16e71c
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-03-04 15:26:22 +0700

    Add Itinerary reservation respository

---
+++ Frypto.Core/Persistences/Repositories/IItineraryReservationRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Frypto.Core.Models;
+
+namespace Frypto.Core.Persistences.Repositories
+{
+    public interface IItineraryReservationRepository
+    {
+        Task Add(ItineraryReservation reservation);
+        Task<ItineraryReservation> Get(int id, bool isInclude = true);
+    }
+}
---
+++ Frypto.Core/Persistences/Repositories/ItineraryReservationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Frypto.Core.Models;
+
+namespace Frypto.Core.Persistences.Repositories
+{
+    public class ItineraryReservationRepository : IItineraryReservationRepository
+    {
+        private readonly FryptoDbContext _context;
+
+        public ItineraryReservationRepository(FryptoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(ItineraryReservation reservation)
+        {
+            _context.ItineraryReservations.Add(reservation);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ItineraryReservation> Get(int id, bool isInclude = true)
+        {
+            var result =  _context.ItineraryReservations.Where(x => x.Id == id);
+
+            if (isInclude)
+            {
+                result = result.Include(x => x.Agent)
+                    .Include(x => x.Passenger)
+                    .Include(x => x.TravelClass);
+            }
+
+            return await result.FirstOrDefaultAsync();
+        }
+    }
+}
