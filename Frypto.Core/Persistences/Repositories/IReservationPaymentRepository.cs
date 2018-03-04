540b8da4f751e8e76bd70cd42bd6e6c7886bd0e0
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-03-04 16:16:22 +0700

    Add reservation payment repository

---
+++ Frypto.Core/Persistences/Repositories/IReservationPaymentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frypto.Core.Models;
+
+namespace Frypto.Core.Persistences.Repositories
+{
+    public interface IReservationPaymentRepository
+    {
+        Task Add(ReservationPayment payment);
+    }
+}
---
+++ Frypto.Core/Persistences/Repositories/ReservationPaymentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frypto.Core.Models;
+
+namespace Frypto.Core.Persistences.Repositories
+{
+    public class ReservationPaymentRepository : IReservationPaymentRepository
+    {
+        private readonly FryptoDbContext _context;
+
+        public ReservationPaymentRepository(FryptoDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(ReservationPayment payment)
+        {
+            _context.ReservationPayments.Add(payment);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
