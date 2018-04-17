b706ebf51ae45bed0e44dca3e61b8853570673f7
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-04-17 21:37:33 +0700

    Add airport controller

---
+++ Frypto/Controllers/AirportsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Frypto.Core.Persistences;
+using Frypto.Core.Persistences.Repositories;
+
+namespace Frypto.Controllers
+{
+    [RoutePrefix("api/Airports")]
+    public class AirportsController : ApiController
+    {
+        private FryptoDbContext Context { get; }
+
+        public AirportsController()
+        {
+            Context = FryptoDbContext.Create();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Context.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private IAirportRepository _airportRepository;
+
+        public IAirportRepository AirportRepository
+        {
+            get
+            {
+                if (_airportRepository == null)
+                {
+                    return _airportRepository = new AirportRepository(Context);
+                }
+                return _airportRepository;
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IHttpActionResult> GetAirport([FromUri]string code)
+        {
+            if (string.IsNullOrEmpty(code) || code.ToCharArray().Length > 3) return BadRequest(code);
+
+            var airports = await AirportRepository.Get(code);
+
+            if (!airports.Any()) return NotFound();
+
+            return Ok(airports);
+        }
+    }
+}
